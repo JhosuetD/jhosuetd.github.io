d071aeb32314f7329e4a9c10a37c1799687acdce
JhosuetD <103470923+JhosuetD@users.noreply.github.com>
2022-08-03 00:56:11 -0500

    Update tarea3.1.3.js

---
+++ Tarea3.1/tarea3.1.3.js
@@ -0,0 +1,20 @@
+/* 3 DESCUENTOS
+Un almacén de escritorios hace los siguientes descuentos: si el
+cliente compramenos de 5 unidadesse le da un descuento del 10%
+sobre la compra;si el número de unidades es mayor o igual a cinco,
+pero menos de 10 se le otorga un
+20% y, si son 10 o más se le da un 40%.*/
+
+var unidades,precio,desc,compra,pagar;
+
+	precio = parseFloat(prompt("Ingresar precio"));
+	unidades = parseFloat(prompt("Ingresar cantidad"));
+    if (unidades >0 ){
+        if(unidades <5)
+        console.log("El total a pagar es de: " << total - ( total * 0.1 );
+         if(unidades > = 5 && unidades < 10)
+        console.log("El total a pagar es de: " << total - ( total * 0.2 );
+        if(unidades > = 10)
+        console.log("El total a pagar es de: " << total - ( total * 0.4 );
+
+    } 
