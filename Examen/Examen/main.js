82fcb486a6fd97007e59f4854ddee2f0347d9cf9
JhosuetD <103470923+JhosuetD@users.noreply.github.com>
2022-08-15 03:28:06 -0500

    Update main.js

--- Examen/Examen/main.js
+++ Examen/Examen/main.js
@@ -1,11 +1,12 @@
 // API thedogapi https://api.thedogapi.com/v1/images/search
 const urlApiDogRandom = 'https://api.thedogapi.com/v1/images/search?limit=4&api_key=1100b599-f09b-4b7f-a110-bb613a8299ea';
-
+//
 //favorite dogs
-const urlApiDogFavorite = 'https://api.thedogapi.com/v1/favourites?3api_key=1100b599-f09b-4b7f-a110-bb613a8299ea';
+const urlApiDogFavorite = 'https://api.thedogapi.com/v1/favourites?4api_key=1100b599-f09b-4b7f-a110-bb613a8299ea';
+
 
 //add dogs
-const urlAddpiDogFavorite = 'https://api.thedogapi.com/v1/favourites?3api_key=1100b599-f09b-4b7f-a110-bb613a8299ea';
+const urlAddpiDogFavorite = 'https://api.thedogapi.com/v1/favourites?4api_key=1100b599-f09b-4b7f-a110-bb613a8299ea';
 
 //remove dogs
 const urlRemovepidogFavorite = 'https://api.thedogapi.com/v1/favourites/';
@@ -36,18 +37,57 @@ const getDogRandom = async () => {
     });
     div.append(...array);
 }
-getDogRandom();
+
 
 
 //funcion para perros favoritos 
-const getDogsFavorite = async () => {
+const getDogFavorite = async () => {
     const response = await fetch (urlApiDogFavorite);
     const data = await response.json();
     const div = document.querySelector('#getDogFavorite__dogs');
     const array = [];
     data.map(catg => {
         const img = document.createElement('img');
-        img.src = catg.img
-    })
+        img.src = catg.image.url;
+        img.alt = catg.user_id;
+        img.className = "img-favorite";
+        img.width = 200;
+        img.height = 200;
+        array.push(img);
 
+        //creando mi button
+        const button = document.createElement('button');
+        button.innerHTML = "Eliminar a favoritos";
+        button.onclick = () =>{
+            removeDogFavorite(catg.id);
+        }
+        array.push(button);
+    });
+    div.append(...array);
 }
+
+// funcion para agregar los  Perros a favoritos 
+
+const addDogFavorite = async (id) =>{
+    const response = await fetch(urlAddpiDogFavorite, {
+        method: "POST" ,
+        headers: {
+            "Content-Type" : 'application/json',
+        },
+        body:JSON.stringify({
+            image_id: id,
+        }),
+    });
+    getDogFavorite();
+}
+//funcion para eliminar de favoritos 
+const removeDogFavorite = async (id) =>{
+    const response = await fetch(`${urlRemovepidogFavorite}${id}?api_key=1100b599-f09b-4b7f-a110-bb613a8299ea`,{
+        method: 'DELETE',
+    });
+    getDogFavorite();
+}
+
+getDogFavorite();
+
+//getDogRandom();
